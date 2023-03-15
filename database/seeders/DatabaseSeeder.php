a6a9652cc25ce4e77c8b23acaeb142629db203db
Prabodashan <prabodashan14@gmail.com>
2023-03-15 13:16:02 +0530

    added listing to userid

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,7 +14,14 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
-        \App\Models\Listing::factory(6)->create();
+
+        $user = User::factory()->create([
+            'name' => 'John Doe',
+            'email' => 'john@example.com',
+        ]);
+        \App\Models\Listing::factory(6)->create([
+            'user_id' => $user->id,
+        ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
