e70202e14ea9566619cde24f02bce5ca7e1866df
Prabodashan <prabodashan14@gmail.com>
2023-03-15 12:23:32 +0530

    added register

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -41,7 +42,11 @@ Route::put('/listings/{listing}', [ListingController::class, 'update']);
 // Delete Listing
 Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
 
-
-
 //single listing 
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
+//Show Register / Create Form
+Route::get('/register', [UserController::class, 'create']);
+
+//Create New User
+Route::post('/users', [UserController::class, 'store']);
